e2419537e863eae582d2ab18d5cc5e2f20e2f068
infinityhacker <825503231@qq.com>
2017-09-08 16:00:14 +0800

    use arping to detect alive hosts

--- arp1.sh
+++ arp1.sh
@@ -1,12 +1,12 @@
-#！/bin/bash
-if ["$#" -ne 1]:then
+！/bin/bash
+if [ "$#" -ne 1 ];then
   echo "Usage ./arping.sh [interface]"
   echo "Example ./arping .sh eth0"
   echo "Example will perform an ARP scan of the local subnet to which etho0 is assigned"
   exit
 fi
-interfac=$1
-prefix=$(ifconfig  $interface | grep "inet addr " | cuy -d ':' -f 2 | cut -d ' ' -f 1 | cut -d '.' -f 1-3)
+interface=$1
+prefix=$(ifconfig  $interface | grep "netmask" | cut -d ' ' -f 10 | cut -d '.' -f 1-3)
 for addr in $(seq 1 254);do
- arping -c 1 $prefix.$addr | grep "bytes from " | cut -d "" -f 5| cut -d "(" -f 2| cut -d ")" -f 1
+ arping -c 1 $prefix.$addr  | grep "Unicast" | cut -d ' ' -f 4
 done
