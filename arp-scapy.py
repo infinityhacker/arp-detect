df38d76f1dd3838a1449efb970ba4d3b530f7899
infinityhacker <825503231@qq.com>
2017-09-08 16:32:25 +0800

    use scapy to detect alive host

--- arp-scapy.py
+++ arp-scapy.py
@@ -6,20 +6,21 @@ import subprocess
 logging.getLogger("scapy.runtime").setLevel(logging.ERROR)
 from scapy.all import *
 
-if len(sys.argv[1:])!=2:
-  print "Usage ./arp_detect.py [interface]"
-  print "Example ./arp_dect.py eth0"
-  print "Example will perform an ARP scan of the local submit to which eth0 is assigned"
-  sys.exit()
-  
- interface=str(sys.argv[1])
+if len(sys.argv[1:])!=1:
+    print "Usage ./arp_detect.py [interface]"
+    print "Example ./arp_dect.py eth0"
+    print "Example will perform an ARP scan of the local submit to which eth0 is assigned"
+    sys.exit()
+
+interface=str(sys.argv[1])
+ip=subprocess.check_output("ifconfig "+interface+" | grep 'netmask' | cut -d ' ' -f 10 | cut -d '.' -f 1-3",shell=True).strip()
+
+prefix=ip.split('.')[0]+"."+ip.split('.')[1]+"."+ip.split('.')[2]+"."
  
- ip=subprocess.check.output("ifconfig"+interface+"| grep 'inet addr | cut -d ':' -f 2 | cut -d ' ' -f 1",shell=True).strip()
- prefix=ip.split('.')[0]+"."+ip.split('.')[1]+"."+ip.split('.')[2]+"."
- 
- for  addr in range(0,254):
-  a=sr1(ARP(pdst=prefix+str(addr)),timeout=0.1,verbose=0)
-  if answer==None:
-    pass
-   else:
-    print prefix+str(addr)
+for  addr in range(0,254):
+    a=sr1(ARP(pdst=prefix+str(addr)),timeout=0.1,verbose=0)
+    if a==None:
+        pass
+    else:
+        print prefix+str(addr)
+     
